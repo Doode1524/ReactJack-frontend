e8746d9cb6801a88b7ad5d6ba60a85dc9d0b7da0
Doode1524 <Doode1524@gmail.com>
2021-03-30 15:16:05 -0400

    App component and index.js init

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Router, Route, Switch } from 'react-router-dom'
+
+const App = () => {
+    return (
+        <div>
+            Hello Blackjack App!
+        </div>
+    )
+}
+
+export default App
