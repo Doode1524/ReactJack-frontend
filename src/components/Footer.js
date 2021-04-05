b8505a7fc89387c19203475a5d7c291e5d9ca6ed
Joe@Edood <Doode1524@gmail.com>
2021-04-05 15:53:21 -0400

    footer container

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react'
 class Footer extends Component {
   render() {
     return (
-      <div>
+      <div className="ui container text center">
         	&copy; 2-Slice Gaming      
       </div>
     )
