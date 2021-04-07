cf2544a75f0f663b130b634f8502fec959553e00
Joe@Edood <Doode1524@gmail.com>
2021-04-07 15:01:30 -0400

    cleanup action

--- src/actions/index.js
+++ src/actions/index.js
@@ -24,29 +24,21 @@ export const togDouble = (wallet) => ({type: TOGGLE_DOUBLE, payload: wallet})
 export const shuffleDeck = () => async dispatch => { 
     const response = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6')
     dispatch({ type: SHUFFLE, payload: response.data })
-    // console.log(response.data)
 }
 
 export const drawTwoCards = (deckId) => async dispatch => { 
     const response = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=3`)
     dispatch({ type: DRAW_TWO, payload: response.data })
-    // debugger
-    // console.log(response.data, 'draw2')
-
 }
 
 export const userDrawOne = (deckId) => async dispatch => { 
     const response = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
     dispatch({ type: USER_DRAW_ONE, payload: response.data })
-    // console.log(response.data)
-
 }
 
 export const dealerDrawOne = (deckId) => async dispatch => { 
     const response = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
     dispatch({ type: DEALER_DRAW_ONE, payload: response.data })
-    // console.log(response.data)
-
 }
 
 export const createUser = (user) => async dispatch => { 
