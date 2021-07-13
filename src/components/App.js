26ce16b9541546d09bbdaa2528aa5144c67795b3
joey <doode1524@gmail.com>
2021-07-13 19:14:56 -0400

    cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -1,119 +1,156 @@
-import React from 'react'
-import history from '../history'
-import { Router, Route, Switch } from 'react-router-dom'
-import { shuffleDeck, currentUser, userWallet } from '../actions'
-import { connect } from 'react-redux'
-import Welcome from './Welcome'
-import Draw from './Draw'
-import SignUp from './SignUp'
-import SignIn from './SignIn'
-import Footer from './Footer';
-import axios from 'axios'
-import Home from './Home'
-import '../app.css'
+import React from "react";
+import history from "../history";
+import { Router, Route, Switch } from "react-router-dom";
+import { shuffleDeck, currentUser, userWallet } from "../actions";
+import { connect } from "react-redux";
+import Welcome from "./Welcome";
+import Draw from "./Draw";
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+import Footer from "./Footer";
+import axios from "axios";
+import Home from "./Home";
+import "../app.css";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            isLoggedIn: false,
-            user: {}
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      user: {},
+    };
+  }
 
-    componentDidMount() {
-        this.loginStatus()
-        return this.props.loggedInStatus ? this.redirect() : null
-    }
+  componentDidMount() {
+    this.loginStatus();
+    return this.props.loggedInStatus ? this.redirect() : null;
+  }
 
-    handleClick = async () => {
-        axios
-          .delete("http://localhost:3001/logout", { withCredentials: true })
-          .then((response) => {
-            this.handleLogout();
-            history.push("/");
-          })
-          .catch((error) => console.log(error));
-        };
-        
-         handlePatch = async () => {
-            this.props.userWallet(this.props.wallet)
-            let user = {
-                wallet: this.props.wallet
-              }
+  handleClick = async () => {
+    axios
+      .delete("http://localhost:3001/logout", { withCredentials: true })
+      .then((response) => {
+        this.handleLogout();
+        history.push("/");
+      })
+      .catch((error) => console.log(error));
+  };
 
-            axios
-              .patch("http://localhost:3001/users/" + this.state.user.id, {user}, { withCredentials: true })
-              .then((response) => {
-                this.handleLogout();
-                history.push("/");
-              })
-              .catch((error) => console.log(error));
+  handlePatch = async () => {
+    this.props.userWallet(this.props.wallet);
+    let user = {
+      wallet: this.props.wallet,
+    };
+
+    axios
+      .patch(
+        "http://localhost:3001/users/" + this.state.user.id,
+        { user },
+        { withCredentials: true }
+      )
+      .then((response) => {
+        this.handleLogout();
+        history.push("/");
+      })
+      .catch((error) => console.log(error));
+  };
+
+  handleLogin = (data) => {
+    this.setState({
+      isLoggedIn: true,
+      user: data.user,
+    });
+    this.props.currentUser(this.state.user);
+  };
+
+  handleLogout = () => {
+    this.setState({
+      isLoggedIn: false,
+      user: {},
+    });
+    history.push("/");
+  };
+
+  loginStatus = () => {
+    axios
+      .get("http://localhost:3001/logged_in", { withCredentials: true })
+
+      .then((response) => {
+        if (response.data.logged_in) {
+          this.handleLogin(response);
+        } else {
+          this.handleLogout();
         }
+      })
+      .catch((error) => console.log("api errors:", error));
+  };
 
-    handleLogin = (data) => {
-        this.setState ({
-            isLoggedIn: true,
-            user: data.user
-        })
-        this.props.currentUser(this.state.user)
-    }
-
-    handleLogout = () => {
-        this.setState({
-            isLoggedIn: false,
-            user: {}
-        })
-        history.push('/')
-    }
-
-    loginStatus = () => {
-        axios.get('http://localhost:3001/logged_in',
-        {withCredentials: true})
-
-        .then(response => {
-            if (response.data.logged_in) {
-                this.handleLogin(response)
-            } else {
-                this.handleLogout()
-            }
-        })
-        .catch(error => console.log('api errors:', error))
-    }
-
-    render() {
+  render() {
     return (
-        <Router history={history}>
-            <div className='app'>
-                <Switch>
-                    <Route exact path='/' render={props => (
-                        <Home {...props} handleLogout={this.handleLogout} loggedInStatus={this.state.isLoggedIn}/>
-                    )} />
-                    <Route path='/login' render={props => (
-                        <SignIn {...props} handleLogin={this.handleLogin}
-                        loggedInStatus={this.state.isLoggedIn} />
-                    )} />
-                    <Route path='/signup' render={props => (
-                        <SignUp {...props} handleLogin={this.handleLogin}
-                        loggedInStatus={this.state.isLoggedIn} />
-                    )} />
-                    <Route path='/start' exact component={Welcome} />
-                    <Route exact path='/play' render={props => (
-                        <Draw {...props} handleLogout={this.handleLogout} handleClick={this.handleClick} handlePatch={this.handlePatch}
-                        loggedInStatus={this.state.isLoggedIn} />
-                    )} />
-                </Switch>
-            </div>
-                <Footer />
-        </Router>
-        )
-    }
+      <Router history={history}>
+        <div className="app">
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <Home
+                  {...props}
+                  handleLogout={this.handleLogout}
+                  loggedInStatus={this.state.isLoggedIn}
+                />
+              )}
+            />
+            <Route
+              path="/login"
+              render={(props) => (
+                <SignIn
+                  {...props}
+                  handleLogin={this.handleLogin}
+                  loggedInStatus={this.state.isLoggedIn}
+                />
+              )}
+            />
+            <Route
+              path="/signup"
+              render={(props) => (
+                <SignUp
+                  {...props}
+                  handleLogin={this.handleLogin}
+                  loggedInStatus={this.state.isLoggedIn}
+                />
+              )}
+            />
+            <Route path="/start" exact component={Welcome} />
+            <Route
+              exact
+              path="/play"
+              render={(props) => (
+                <Draw
+                  {...props}
+                  handleLogout={this.handleLogout}
+                  handleClick={this.handleClick}
+                  handlePatch={this.handlePatch}
+                  loggedInStatus={this.state.isLoggedIn}
+                />
+              )}
+            />
+          </Switch>
+        </div>
+        <Footer />
+      </Router>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        wallet: state.user.wallet,
-    }
-}
+  return {
+    wallet: state.user.wallet,
+  };
+};
 
-export default connect(mapStateToProps, { shuffleDeck, currentUser, userWallet })(App)
+export default connect(mapStateToProps, {
+  shuffleDeck,
+  currentUser,
+  userWallet,
+})(App);
--- src/components/DealerDrawnCards.js
+++ src/components/DealerDrawnCards.js
@@ -1,26 +1,35 @@
-import React from 'react'
+import React from "react";
 
-const DealerDrawnCards = ({pushUserValues, pushDealerValues, handleDealerAces, dealerCards, dealerValues, userValues, dealerDrawOne, deckId}) => {
-    
-    pushUserValues();
-    pushDealerValues();
-    handleDealerAces();
+const DealerDrawnCards = ({
+  pushUserValues,
+  pushDealerValues,
+  handleDealerAces,
+  dealerCards,
+  dealerValues,
+  userValues,
+  dealerDrawOne,
+  deckId,
+}) => {
+  pushUserValues();
+  pushDealerValues();
+  handleDealerAces();
 
-    if (
-        dealerCards &&
-        dealerValues.reduce((a, b) => a + b, 0) < 17 &&
-        dealerCards.length > 1 &&
-        dealerValues.reduce((a, b) => a + b, 0) <=
-        userValues.reduce((a, b) => a + b, 0)
-    ) { dealerDrawOne(deckId);}
+  if (
+    dealerCards &&
+    dealerValues.reduce((a, b) => a + b, 0) < 17 &&
+    dealerCards.length > 1 &&
+    dealerValues.reduce((a, b) => a + b, 0) <=
+      userValues.reduce((a, b) => a + b, 0)
+  ) {
+    dealerDrawOne(deckId);
+  }
 
-    return (
-        dealerCards &&
-        dealerCards.map((card, i) => (
-        <img width="200" height="250" src={card.image} key={i} />
+  return (
+    dealerCards &&
+    dealerCards.map((card, i) => (
+      <img width="200" height="250" src={card.image} key={i} />
     ))
   );
+};
 
-}
-
-export default DealerDrawnCards
+export default DealerDrawnCards;
